6a21e2b6c27cbc5f02f50b2d716ca1e19e81139b
Ittipol k <Ittipol.Kaewprasert@krungsri.com>
2023-05-06 22:27:46 +0700

    Update README.md & implement gRPC server

--- server/main.go
+++ server/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"server/services"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
+	// Start gRPC Server
+	server := grpc.NewServer()
+
+	listener, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		panic(err)
+	}
+
+	srv := services.NewCalculatorServer()
+	services.RegisterCalculatorServer(server, srv)
+
+	fmt.Println("gRPC server start")
+	err = server.Serve(listener)
+
+	fmt.Println(err)
+
+	if err != nil {
+		panic(err)
+	}
 
 }
