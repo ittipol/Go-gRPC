0d903e0988f698dc6e685103ccf124ccc05446ac
Ittipol k <Ittipol.Kaewprasert@krungsri.com>
2023-05-08 15:19:57 +0700

    update docker-compose.yml & implement secure connection at gRPC server

--- client/main.go
+++ client/main.go
@@ -2,36 +2,75 @@ package main
 
 import (
 	"client/services"
+	"flag"
 	"fmt"
+	"strings"
 
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 )
 
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+}
+
 func main() {
 
-	cred := insecure.NewCredentials()
+	var (
+		cc    *grpc.ClientConn
+		err   error
+		creds credentials.TransportCredentials
+	)
 
-	cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(cred))
-	defer cc.Close()
+	host := flag.String("host", "localhost:50051", "gRPC Server host")
+	tls := flag.Bool("tls", false, "Use TLS Connection")
+	flag.Parse()
+
+	creds = insecure.NewCredentials()
+
+	if flag.Parsed() && *tls {
+		cert := viper.GetString("tls.cert")
+
+		fmt.Printf("CERT: %v \n", cert)
+
+		creds, err = credentials.NewClientTLSFromFile(cert, "")
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	cc, err = grpc.Dial(*host, grpc.WithTransportCredentials(creds))
 
 	if err != nil {
 		panic(err)
 	}
 
+	defer cc.Close()
+
 	accountClient := services.NewAccountClient(cc)
 	accountService := services.NewAccountService(accountClient)
 
-	err = accountService.User("")
+	// err = accountService.User("Hello")
 	// err = accountService.Fibonacci(4)
-	// err = accountService.Average(3, 4, 5, 6, 7, 8, 9, 20, 100, 10000)
+	err = accountService.Average(3, 4, 5, 6, 7, 8, 9, 20, 100, 10000)
 	// err = accountService.Sum(3, 4, 5, 6, 7, 8, 9, 20, 100, 10000)
 
 	if err != nil {
 
 		if grpcError, ok := status.FromError(err); ok {
-			fmt.Printf("Is gRPC Error \n")
+			fmt.Printf("gRPC occured an error\n")
 			fmt.Printf("%v | %v | %v \n", grpcError.Code(), grpcError.Details(), grpcError.Message())
 			panic(grpcError)
 		}
--- server/main.go
+++ server/main.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"server/services"
+	"strings"
 
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 )
 
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+}
+
 func main() {
 
-	server := grpc.NewServer()
+	var server *grpc.Server
+
+	// Receive value from Flag in command-line
+	tls := flag.Bool("tls", false, "Use TLS Connection") // Create Flag name tls
+	flag.Parse()
+	// See list of flag after parsed
+	// go run . -h
+
+	if flag.Parsed() && *tls {
+		cert := viper.GetString("tls.cert")
+		key := viper.GetString("tls.key")
+
+		fmt.Printf("CERT: %v | KEY: %v \n", cert, key)
+
+		creds, err := credentials.NewServerTLSFromFile(cert, key)
+
+		if err != nil {
+			panic(err)
+		}
+
+		server = grpc.NewServer(grpc.Creds(creds))
+
+		fmt.Println("Connection is secure")
+	} else {
+		// insecure
+		server = grpc.NewServer()
+
+		fmt.Println("Connection is not secure")
+	}
+
 	defer server.Stop()
 
 	listener, err := net.Listen("tcp", ":50051")
@@ -23,7 +68,7 @@ func main() {
 	svc := services.NewAccountServer()
 	services.RegisterAccountServer(server, svc)
 	// Set reflection
-	// usage command
+	// Usage command
 	// evans --reflection
 	reflection.Register(server)
 
